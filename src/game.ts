c7cc71f6d93b19f473fa42ad57b13a1ec481f587
nearnshaw <nearnshaw@decentraland.org>
2021-02-12 12:53:01 -0300

    new utils lib version

--- src/game.ts
+++ src/game.ts
@@ -1,66 +1,104 @@
-import { Portal, PortalColor } from "./portal"
-import { Card } from "./card"
-import { Gun } from "./gun"
-import { Sound } from "./sound"
-import utils from "../node_modules/decentraland-ecs-utils/index"
+import { Portal, PortalColor } from './portal'
+import { Card } from './card'
+import { Gun } from './gun'
+import { Sound } from './sound'
+import * as utils from '@dcl/ecs-scene-utils'
+import { movePlayerTo } from '@decentraland/RestrictedActions'
 
 // Base
 const base = new Entity()
-base.addComponent(new GLTFShape("models/baseLight.glb"))
+base.addComponent(new GLTFShape('models/baseLight.glb'))
 engine.addEntity(base)
 
 // Walls
 const walls = new Entity()
-walls.addComponent(new GLTFShape("models/walls.glb"))
+walls.addComponent(new GLTFShape('models/walls.glb'))
 walls.addComponent(new Transform())
 engine.addEntity(walls)
 
 // Gun
 const gunBlueGlow = new Entity()
 gunBlueGlow.addComponent(new Transform())
-gunBlueGlow.addComponent(new GLTFShape("models/portalGunBlueGlow.glb"))
+gunBlueGlow.addComponent(new GLTFShape('models/portalGunBlueGlow.glb'))
 const gunOrangeGlow = new Entity()
 gunOrangeGlow.addComponent(new Transform())
-gunOrangeGlow.addComponent(new GLTFShape("models/portalGunOrangeGlow.glb"))
+gunOrangeGlow.addComponent(new GLTFShape('models/portalGunOrangeGlow.glb'))
 gunOrangeGlow.getComponent(Transform).scale.setAll(0) // Hide orange glow
-const gun = new Gun(new GLTFShape("models/portalGun.glb"), new Transform({ position: new Vector3(8, 1.5, 4.5) }), gunBlueGlow, gunOrangeGlow)
+const gun = new Gun(
+  new GLTFShape('models/portalGun.glb'),
+  new Transform({ position: new Vector3(8, 1.5, 4.5) }),
+  gunBlueGlow,
+  gunOrangeGlow
+)
 
 // Card
-const card = new Card(new GLTFShape("models/card.glb"), new Transform({ position: new Vector3(8, 6.75, 13.5) }))
+const card = new Card(
+  new GLTFShape('models/card.glb'),
+  new Transform({ position: new Vector3(8, 6.75, 13.5) })
+)
 
 // Sounds
-const teleportSound = new Sound(new AudioClip("sounds/teleport.mp3"))
-const portalSuccessSound = new Sound(new AudioClip("sounds/portalSuccess.mp3"))
-const portalFailSound = new Sound(new AudioClip("sounds/portalFail.mp3"))
+const teleportSound = new Sound(new AudioClip('sounds/teleport.mp3'))
+const portalSuccessSound = new Sound(new AudioClip('sounds/portalSuccess.mp3'))
+const portalFailSound = new Sound(new AudioClip('sounds/portalFail.mp3'))
 
 // Portals
-const portalOrange = new Portal(new GLTFShape("models/portalOrange.glb"))
-const portalBlue = new Portal(new GLTFShape("models/portalBlue.glb"))
+const portalOrange = new Portal(new GLTFShape('models/portalOrange.glb'))
+const portalBlue = new Portal(new GLTFShape('models/portalBlue.glb'))
 const DELAY_TIME = 1500 // In milliseconds
 const RESET_SIZE = 2 // In meters
 
-let triggerBox = new utils.TriggerBoxShape(new Vector3(RESET_SIZE, RESET_SIZE, RESET_SIZE), Vector3.Zero())
+let triggerBox = new utils.TriggerBoxShape(
+  new Vector3(RESET_SIZE, RESET_SIZE, RESET_SIZE),
+  Vector3.Zero()
+)
 
 portalBlue.addComponent(
-  new utils.TriggerComponent(triggerBox, null, null, null, null, () => {
-    if (portalOrange.hasComponent(Transform)) {
-      teleportSound.getComponent(AudioSource).playOnce()
-      movePlayerTo(portalOrange.getComponent(Transform).position, portalOrange.cameraTarget)
-      triggerBox.size.setAll(0) // Resize the trigger so that the player doesn't port in and out constantly
-      portalOrange.addComponent(new utils.Delay(DELAY_TIME, () => { triggerBox.size.setAll(RESET_SIZE) })) // Reset the trigger after 1.5 seconds
-      portalBlue.addComponent(new utils.Delay(DELAY_TIME, () => { triggerBox.size.setAll(RESET_SIZE) }))
-    }
+  new utils.TriggerComponent(triggerBox, {
+    onCameraEnter: () => {
+      if (portalOrange.hasComponent(Transform)) {
+        teleportSound.getComponent(AudioSource).playOnce()
+        movePlayerTo(
+          portalOrange.getComponent(Transform).position,
+          portalOrange.cameraTarget
+        )
+        triggerBox.size.setAll(0) // Resize the trigger so that the player doesn't port in and out constantly
+        portalOrange.addComponent(
+          new utils.Delay(DELAY_TIME, () => {
+            triggerBox.size.setAll(RESET_SIZE)
+          })
+        ) // Reset the trigger after 1.5 seconds
+        portalBlue.addComponent(
+          new utils.Delay(DELAY_TIME, () => {
+            triggerBox.size.setAll(RESET_SIZE)
+          })
+        )
+      }
+    },
   })
 )
 portalOrange.addComponent(
-  new utils.TriggerComponent(triggerBox, null, null, null, null, () => {
-    if (portalBlue.hasComponent(Transform)) {
-      teleportSound.getComponent(AudioSource).playOnce()
-      movePlayerTo(portalBlue.getComponent(Transform).position, portalBlue.cameraTarget)
-      triggerBox.size.setAll(0)
-      portalOrange.addComponent(new utils.Delay(DELAY_TIME, () => { triggerBox.size.setAll(RESET_SIZE) }))
-      portalBlue.addComponent(new utils.Delay(DELAY_TIME, () => { triggerBox.size.setAll(RESET_SIZE) }))
-    }
+  new utils.TriggerComponent(triggerBox, {
+    onCameraEnter: () => {
+      if (portalBlue.hasComponent(Transform)) {
+        teleportSound.getComponent(AudioSource).playOnce()
+        movePlayerTo(
+          portalBlue.getComponent(Transform).position,
+          portalBlue.cameraTarget
+        )
+        triggerBox.size.setAll(0)
+        portalOrange.addComponent(
+          new utils.Delay(DELAY_TIME, () => {
+            triggerBox.size.setAll(RESET_SIZE)
+          })
+        )
+        portalBlue.addComponent(
+          new utils.Delay(DELAY_TIME, () => {
+            triggerBox.size.setAll(RESET_SIZE)
+          })
+        )
+      }
+    },
   })
 )
 
@@ -69,9 +107,10 @@ const input = Input.instance
 let activePortal = PortalColor.Blue
 const HEIGHT_ABOVE_GROUND = 1.2 // In meters
 
-input.subscribe("BUTTON_DOWN", ActionButton.POINTER, true, (event) => {
+input.subscribe('BUTTON_DOWN', ActionButton.POINTER, true, (event) => {
   if (gun.hasGun) {
-    if (event.hit.meshName.match("lightWall_collider")) { // Only allow portals to appear on light walls
+    if (event.hit.meshName.match('lightWall_collider')) {
+      // Only allow portals to appear on light walls
       portalSuccessSound.getComponent(AudioSource).playOnce()
 
       if (activePortal == PortalColor.Blue) {
@@ -80,7 +119,8 @@ input.subscribe("BUTTON_DOWN", ActionButton.POINTER, true, (event) => {
         transform.lookAt(event.hit.normal)
         transform.position = event.hit.hitPoint
         portalBlue.cameraTarget = transform.position.add(event.hit.normal)
-        if (transform.position.y <= HEIGHT_ABOVE_GROUND) transform.position.y = HEIGHT_ABOVE_GROUND // Make sure the portal is above ground height
+        if (transform.position.y <= HEIGHT_ABOVE_GROUND)
+          transform.position.y = HEIGHT_ABOVE_GROUND // Make sure the portal is above ground height
         portalBlue.playAnimation()
       } else {
         portalOrange.addComponentOrReplace(new Transform())
@@ -88,7 +128,8 @@ input.subscribe("BUTTON_DOWN", ActionButton.POINTER, true, (event) => {
         transform.lookAt(event.hit.normal)
         transform.position = event.hit.hitPoint
         portalOrange.cameraTarget = transform.position.add(event.hit.normal)
-        if (transform.position.y <= HEIGHT_ABOVE_GROUND) transform.position.y = HEIGHT_ABOVE_GROUND
+        if (transform.position.y <= HEIGHT_ABOVE_GROUND)
+          transform.position.y = HEIGHT_ABOVE_GROUND
         portalOrange.playAnimation()
       }
     } else {
@@ -98,7 +139,7 @@ input.subscribe("BUTTON_DOWN", ActionButton.POINTER, true, (event) => {
 })
 
 // Swap between portal colors when pressing the E key
-input.subscribe("BUTTON_DOWN", ActionButton.PRIMARY, false, (): void => {
+input.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, false, (): void => {
   if (activePortal == PortalColor.Blue) {
     activePortal = PortalColor.Orange
     gunBlueGlow.getComponent(Transform).scale.setAll(0)
--- src/gun.ts
+++ src/gun.ts
@@ -1,18 +1,24 @@
-import { Sound } from "./sound"
-import utils from "../node_modules/decentraland-ecs-utils/index"
-import { TriggerBoxShape } from "../node_modules/decentraland-ecs-utils/triggers/triggerSystem"
+import { Sound } from './sound'
+import * as utils from '@dcl/ecs-scene-utils'
 
-const gunPickupSound = new Sound(new AudioClip("sounds/gunPickup.mp3"))
+const gunPickupSound = new Sound(new AudioClip('sounds/gunPickup.mp3'))
 
 export class Gun extends Entity {
   hasGun: boolean = false
 
-  constructor(model: GLTFShape, transform: Transform, blueGlow: Entity, orangeGlow: Entity) {
+  constructor(
+    model: GLTFShape,
+    transform: Transform,
+    blueGlow: Entity,
+    orangeGlow: Entity
+  ) {
     super()
     engine.addEntity(this)
     this.addComponent(model)
     this.addComponent(transform)
-    this.addComponent(new utils.KeepRotatingComponent(Quaternion.Euler(0, 180, 0)))
+    this.addComponent(
+      new utils.KeepRotatingComponent(Quaternion.Euler(0, 180, 0))
+    )
 
     blueGlow.setParent(this)
     orangeGlow.setParent(this)
@@ -20,16 +26,21 @@ export class Gun extends Entity {
     // Create trigger for card
     this.addComponent(
       new utils.TriggerComponent(
-        new TriggerBoxShape(new Vector3(2, 2, 2), Vector3.Zero()),
-        null, null, null, null, // Default trigger params
-        () => {
-          // Camera enter
-          this.hasGun = true
-          this.setParent(Attachable.FIRST_PERSON_CAMERA)
-          this.getComponent(Transform).position = new Vector3(0.33, 0.33, 1)
-          this.getComponent(Transform).rotation.setEuler(0, 0, 0)
-          this.getComponent(utils.KeepRotatingComponent).stop()
-          gunPickupSound.getComponent(AudioSource).playOnce()
+        new utils.TriggerBoxShape(new Vector3(2, 2, 2)),
+
+        {
+          onCameraEnter: () => {
+            this.hasGun = true
+            this.setParent(Attachable.FIRST_PERSON_CAMERA)
+            this.getComponent(Transform).position = new Vector3(
+              0.45,
+              -0.425,
+              0.9
+            )
+            this.getComponent(Transform).rotation.setEuler(0, 0, 0)
+            this.getComponent(utils.KeepRotatingComponent).stop()
+            gunPickupSound.getComponent(AudioSource).playOnce()
+          },
         }
       )
     )
